c68268a963b4ef1aa94b012ae19b5b65767f5beb
Sergei Miroevski <oleg.3day@protonmail.com>
2022-05-28 21:14:17 +0300

    Complited "27" https://stepik.org/lesson/416757/step/8?auth=login&unit=406265

---
+++ lesson_27.py
@@ -0,0 +1,11 @@
+num = input().split()
+s = [i for i in range(1, int(num[0]) * int(num[1]) + 1)]
+c = []
+for i in range(int(num[0])):
+    c.append(s[:int(num[1])])
+    s = s[int(num[1]):]
+    if i % 2 != 0:
+        c[i].reverse()
+    for j in range(int(num[1])):
+        print(str(c[i][j]).ljust(3), end='')
+    print()
